33abee9834536703b09057502d392cff75ffa36e
Justin Blake <justin@hentzia.com>
2015-09-02 15:14:03 -0400

    Warn future about importing __init__ in setup.py
    See conversation here:
    https://github.com/yola/yolapy/pull/5#discussion_r38526300

--- yolapy/__init__.py
+++ yolapy/__init__.py
@@ -1,1 +1,4 @@
+# setup.py depends on being able to import this without worrying about all
+# required packages being installed. Keep that in mind if you find yourself
+# wanting to add imports here.
 __version__ = '0.1.0'
