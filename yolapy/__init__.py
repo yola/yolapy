b00ed8ee1b2b61c2e67b21b66741e4db49e9260f
Justin Blake <justin@hentzia.com>
2015-09-03 05:47:47 -0400

    Improve comment explaining __init__ import issue
    Imports in __init__ are common, so be more explicit with the warning.

--- yolapy/__init__.py
+++ yolapy/__init__.py
@@ -1,4 +1,4 @@
-# setup.py depends on being able to import this without worrying about all
-# required packages being installed. Keep that in mind if you find yourself
-# wanting to add imports here.
+# setup.py needs to import this without worrying required packages being
+# installed. So if you add imports here, you may need to do something like:
+# https://github.com/mitsuhiko/click/blob/da080dd2de1a116fc5789ffdc1ec6ffb864f2044/setup.py#L1-L11
 __version__ = '0.1.0'
