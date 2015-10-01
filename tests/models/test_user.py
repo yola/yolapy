fdf3fe1d5090a2fa535ddd4d2a11c49c402bf0dc
Doug Beck <doug@douglasbeck.com>
2015-10-01 15:50:16 -0400

    Revert "Use preferences to store the user's email."
    This reverts commit 1bb5281ecf4f4603f481c8cddfaf1203300ffa01.
    
    The client should not be cleaning up the API.

--- tests/models/test_user.py
+++ tests/models/test_user.py
@@ -20,62 +20,20 @@ class TestUserClient(UserTestCase):
         self.assertEqual(User().client, self.client)
 
 
-class TestUserEmail(UserTestCase):
-
-    def setUp(self):
-        super(TestUserEmail, self).setUp()
-        self.user = User(email='me@example.com')
-
-    def test_is_set_as_a_preference(self):
-        self.assertEqual(self.user.preferences['wl_email'], 'me@example.com')
-
-    def test_is_not_destroyed_when_saving(self):
-        self.client.create_user.return_value = {
-            'email': 'legacy-1234567890asdf1234567890asdf.com'
-        }
-        self.user.save()
-        self.assertEqual(self.user.email, 'me@example.com')
-
-
-class TestUserLegacyEmail(UserTestCase):
-
-    def setUp(self):
-        super(TestUserLegacyEmail, self).setUp()
-        self.user = User(name='Jo', surname='Jo', email='me@example.com')
-        self.user._legacy_email = 'legacy-1234567890asdf1234567890asdf.com'
-
-    def test_is_used_when_saving(self):
-        self.user.save()
-        save_call = self.client.create_user.call_args[1]
-        self.assertEqual(save_call, {
-            'name': 'Jo',
-            'surname': 'Jo',
-            'partner_id': 'WL_DEFAULT',
-            'email': 'legacy-1234567890asdf1234567890asdf.com',
-            'preferences': {'wl_email': 'me@example.com'},
-        })
-
-
 class TestUserSave(UserTestCase):
 
     def test_uses_service_to_create_a_new_user(self):
         user_attrs = {
             'name': 'First',
             'surname': 'Name',
-            'email': 'legacy-abc123@example.com',
+            'email': 'email@example.com',
             'partner_id': 'WL_PARTNER',
-            'preferences': {'locale': 'en', 'wl_email': 'email@example.com'},
+            'preferences': {'locale': 'en'},
         }
         user = User(**user_attrs)
-        user._legacy_email = 'legacy-abc123@example.com'
         user.save()
         create_user_call = self.client.create_user.call_args[1]
         self.assertEqual(user_attrs, create_user_call)
-
-    def test_generates_a_legacy_email(self):
-        User().save()
-        create_user_call = self.client.create_user.call_args[1]
-        self.assertTrue(create_user_call['email'] is not None)
 
 
 class TestUserPartnerId(UserTestCase):
