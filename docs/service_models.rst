93264190228e79c85dc6eb0111b63126e27ef652
Doug Beck <doug@douglasbeck.com>
2015-11-05 15:49:52 -0500

    Reformat doctrings for Sphinx

---
+++ docs/service_models.rst
@@ -0,0 +1,8 @@
+Service Models
+==============
+
+Subscription
+-------------
+
+.. autoclass:: yolapy.models.Subscription
+   :members:
--- yolapy/models/subscription.py
+++ yolapy/models/subscription.py
@@ -3,31 +3,32 @@ from yolapy.services import Yola
 
 class Subscription(object):
 
+    """Construct a Subscription.
+
+    The data in a Subscription instance does not persist until it has
+    been saved.
+
+    :param auto_renew: Bool
+    :param billing_date: Datetime
+    :param deprovision_date: Datetime
+    :param expiry_date: Datetime
+    :param id: Str, subscription's 32 char ID
+    :param properties: Dict, subscription properties. If unset will be
+        defaulted to the client's configured username:
+        ``{"partner_id": "<CLIENT_USERNAME>"}``.
+    :param sku: Int
+    :param type: Str, subscription type
+    :param user_id: Str, 32 character ID of the sub's user
+
+    :return: Subscription
+    :rtype: yolapy.models.Subscription
+
+    """
+
     def __init__(
             self, auto_renew=False, billing_date=None, deprovision_date=None,
             expiry_date=None, id=None, properties=None, sku=None,
             start_date=None, status=None, term=None, type=None, user_id=None):
-        """Construct a Subscription.
-
-        The data in a Subscription instance does not persist until it has
-        been saved.
-
-        :param auto_renew: Bool
-        :param billing_date: Datetime
-        :param deprovision_date: Datetime
-        :param expiry_date: Datetime
-        :param id: Str, subscription's 32 char ID
-        :param properties: Dict, subscription properties. If unset will be
-            defaulted to the client's configured username:
-            ```{"partner_id": "<CLIENT_USERNAME>"}```.
-        :param sku: Int
-        :param type: Str, subscription type
-        :param user_id: Str, 32 character ID of the sub's user
-
-        :return: :class:`Subscription <Subscription>` object
-        :rtype: yolapy.subscription.models.Subscription
-
-        """
         self.client = Yola()
 
         self.auto_renew = auto_renew
@@ -43,16 +44,15 @@ class Subscription(object):
     def list(cls, **kwargs):
         """Return a filtered list of Subscriptions.
 
-        Example use:
-        ```
-        from yolapy.models import Subscription
+        Useage::
 
-        user_id = 'abcdef1234567890abcdef1234567890'
-        user_subs = Subscription.list(user_id=user_id)
-
-        print user_subs[0]  # <yolapy.models.subscription.Subscription object>
-        print user_subs[0].user_id  # u'abcdef1234567890abcdef1234567890'
-        ```
+           >>> from yolapy.models import Subscription
+           >>> user_id = 'abcdef1234567890abcdef1234567890'
+           >>> user_subs = Subscription.list(user_id=user_id)
+           >>> print user_subs[0]
+           <yolapy.models.subscription.Subscription object>
+           >>> print user_subs[0].user_id
+           u'abcdef1234567890abcdef1234567890'
         """
         response = Yola().list_subscriptions(**kwargs)
         return [cls(**sub) for sub in response['results']]
