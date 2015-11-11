4e1dea31ff892b1903da92a5880d56b42376a179
Justin Blake <justin@hentzia.com>
2015-11-11 06:37:47 -0500

    pep fixes

--- shell.py
+++ shell.py
@@ -6,10 +6,10 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 
 from yolapy.services import Yola
-from tests.test_integration.test_settings import url, auth
+from tests.test_integration.test_settings import auth, url
 
 
 yola = Yola(url=url, auth=auth)
 
-print "The current directory has been added to the beginning of sys.path"
-print "and the Yola service client has been instantiated as `yola`."
+print 'The current directory has been added to the beginning of sys.path'
+print 'and the Yola service client has been instantiated as `yola`.'
