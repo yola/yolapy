a847f8322ac2e81a722bcb491f28a3087d43199e
Justin Blake <justin@hentzia.com>
2016-03-24 06:13:21 -0400

    Add Site.published_domain

--- yolapy/models/site.py
+++ yolapy/models/site.py
@@ -5,10 +5,16 @@ class Site(object):
     """Represents a Site resource on the Yola API."""
 
     def __init__(self, **kwargs):
+        self.publishing_data = kwargs.pop('publishing_data', {})
         for key, val in kwargs.iteritems():
             setattr(self, key, val)
 
     @classmethod
     def get(cls, site_id):
+        """Get a site from the Yola API."""
         site_attributes = Yola().get_site(site_id)
         return Site(**site_attributes)
+
+    @property
+    def published_domain(self):
+        return self.publishing_data.get('canonical_host')
