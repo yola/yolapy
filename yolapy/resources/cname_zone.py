0e731542f1fa3c406fc40c56adcfc24781e4e7e6
Eugene Koval <eugene@yola.com>
2017-04-13 16:38:16 +0300

    Removed needless method

--- yolapy/resources/cname_zone.py
+++ yolapy/resources/cname_zone.py
@@ -2,9 +2,5 @@ class CnameZoneMixin(object):
     """Methods for managing Cname Zones."""
     def create_cname_zone(self, hostname, partner_id):
         return self.post(
-            self._cname_zones_path(),
+            '/cname-dns-zones/',
             json={'partner_id': partner_id, 'hostname': hostname}).json()
-
-    def _cname_zones_path(self, *parts):
-        path = '/'.join(['cname-dns-zones'] + list(parts))
-        return '/%s/' % path
